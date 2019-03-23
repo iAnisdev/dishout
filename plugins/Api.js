30d421ab04c7a4c6b1d71b2955c75457557313b1
Anis <marahmanjs@gmail.com>
2019-03-24 03:13:07 +0500

    Api header

--- plugins/Api.js
+++ plugins/Api.js
@@ -2,6 +2,10 @@ import axios from 'axios'
 
 export default () => {
     return axios.create({
+        headers: {
+        'Authorization': 'Token 5822cd005a14cf7212bffb51c2bab69d87460dae',
+        'Content-Type': 'application/json'
+        },
         baseURL: 'https://www.dishout.io/api/v1'
     })
 }
